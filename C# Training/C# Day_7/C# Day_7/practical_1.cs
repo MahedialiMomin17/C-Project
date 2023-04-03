fdbdb0290231159c7eb99df0e828d708a474aa7a
mahediali momin <mahedialimomin17@gmail.com>
2023-04-03 10:36:49 +0530

    all changes done

---
+++ C# Training/C# Day_7/C# Day_7/practical_1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Day_7
+{
+    internal class practical_1
+    {
+
+        //static List<string> GroupNumbers(List<int> numbers)
+        //{
+        //    List<string> result = new List<string>();
+        //    if (numbers == null || numbers.Count == 0)
+        //    {
+        //        return result;
+        //    }
+
+        //    numbers.Sort();
+        //    int start = numbers[0];
+        //    int end = start;
+
+        //    for (int i = 1; i < numbers.Count; i++)
+        //    {
+        //        if (numbers[i] == end + 1)
+        //        {
+        //            end = numbers[i];
+        //        }
+        //        else
+        //        {
+        //            if (start == end)
+        //            {
+        //                result.Add(start.ToString());
+        //            }
+        //            else
+        //            {
+        //                result.Add(start + "-" + end);
+        //            }
+
+        //            start = end = numbers[i];
+        //        }
+        //    }
+
+        //    if (start == end)
+        //    {
+        //        result.Add(start.ToString());
+        //    }
+        //    else
+        //    {
+        //        result.Add(start + "-" + end);
+        //    }
+
+        //    return result;
+        //}
+
+ 
+        static string FormatGroup(int start, int end)
+        {
+            return start == end ? start.ToString() : string.Format("{0}-{1}", start, end);
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Enter the number");
+                int caseSwitch = Convert.ToInt32(Console.ReadLine());
+
+                switch (caseSwitch)
+                {
+                    case 1:
+
+                        List<int> originalList = new List<int>() { 1, 2, 3, 4, 0 };
+
+                        List<int> modifiedList = new List<int>();
+
+                        // Loop through the original list and modify each value
+                        foreach (int value in originalList)
+                        {
+                            int newValue = (value + 2) * 5;
+                            modifiedList.Add(newValue);
+                        }
+
+
+                        // Print out the original and modified lists
+                        Console.WriteLine("Original List: ");
+                        foreach (int value in originalList)
+                        {
+                            Console.Write(value + " ");
+                        }
+                        Console.WriteLine();
+
+
+                        Console.WriteLine("Modified List: ");
+                        foreach (int value in modifiedList)
+                        {
+                            Console.Write(value + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+
+
+                    case 2:
+                        //Console.Write("Enter a list of numbers (comma-separated): ");
+                        //string input1 = Console.ReadLine();
+
+                        //List<int> numbers = new List<int>();
+                        //foreach (string str in input1.Split(','))
+                        //{
+                        //    if (int.TryParse(str.Trim(), out int num))
+                        //    {
+                        //        numbers.Add(num);
+                        //    }
+                        //}
+
+                        //List<string> groups = GroupNumbers(numbers);
+
+                        //Console.WriteLine("Result: " + string.Join(", ", groups));
+
+
+
+
+                        //Console.Write("Enter a list of numbers (comma-separated): ");
+                        //string input1 = Console.ReadLine();
+
+                        //List<int> numbers = new List<int>();
+                        //foreach (string str in input1.Split(','))
+                        //{
+                        //    if (int.TryParse(str.Trim(), out int num))
+                        //    {
+                        //        numbers.Add(num);
+                        //    }
+                        //}
+
+                        //numbers.Sort();
+
+                        //List<string> result = new List<string>();
+
+                        //int start = numbers[0];
+                        //int end = start;
+
+                        //for (int i = 1; i < numbers.Count; i++)
+                        //{
+                        //    if (numbers[i] == end + 1)
+                        //    {
+                        //        end = numbers[i];
+                        //    }
+                        //    else
+                        //    {
+                        //        result.Add(start == end ? start.ToString() : $"{start}-{end}");
+
+                        //        start = end = numbers[i];
+                        //    }
+                        //}
+
+                        //result.Add(start == end ? start.ToString() : $"{start}-{end}");
+
+                        //Console.WriteLine("Result: " + string.Join(", ", result));
+
+
+
+                        {
+                            Console.WriteLine("Enter a comma-separated list of page numbers:");
+                            string inputvalue = Console.ReadLine();
+
+                            int[] pages = inputvalue.Split(',').Select(p => int.Parse(p.Trim())).ToArray();
+                            var groups = new List<string>();
+                            for (int i = 0; i < pages.Length; i++)
+                            {
+                                int start = pages[i];
+                                while (i < pages.Length - 1 && pages[i + 1] == pages[i] + 1)
+                                {
+                                    i++;
+                                }
+                                groups.Add(FormatGroup(start, pages[i]));
+                            }
+                            Console.WriteLine("Original: {0}", string.Join(", ", pages.Select(p => p.ToString())));
+                            Console.WriteLine("Grouped:  {0}", string.Join(", ", groups.ToArray()));
+                            Console.ReadKey();
+                        }
+                        break;
+
+
+                    case 3:
+
+                        List<string> students = new List<string>();
+                        int numStudents = 0;
+                       
+
+                        while (true)
+                        {
+                            Console.Write("Enter your name (or 'quit' to exit): ");
+                            Console.WriteLine("");
+                            string name = Console.ReadLine();
+
+                            if (name.ToLower() == "quit")
+                            {
+                                break;
+                            }
+
+                            Console.Write("Enter your age: ");
+                            int age = int.Parse(Console.ReadLine());
+
+                            Console.Write("Enter your GPA: ");
+                            double gpa = double.Parse(Console.ReadLine());
+
+                            if (age >= 18)
+                            {
+                                Console.WriteLine("Congratulations, " + name + "!");
+                                Console.WriteLine("");
+                                students.Add(name);
+                                numStudents++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Sorry, " + name + ",");
+                                Console.WriteLine("");
+
+                            }
+                        }
+
+                        Console.WriteLine("Total number of students admitted: " + numStudents);
+                        Console.WriteLine("");
+
+
+
+                        // Remove students
+                        Console.Write("Enter the name of a student to remove (or 'done' to continue): ");
+                        string input = Console.ReadLine();
+                        while (input.ToLower() != "done")
+                        {
+                            if (students.Remove(input))
+                            {
+                                Console.WriteLine("Removed " + input + " from the college.");
+                                Console.WriteLine("");
+                                numStudents--;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Could not find " + input + " in the college.");
+                            }
+
+                            Console.Write("Enter the name of a student to remove (or 'done' to continue): ");
+                            Console.WriteLine("");
+                            input = Console.ReadLine();
+                        }
+
+                        Console.WriteLine("Total number of students remaining: " + numStudents);
+                        Console.WriteLine("");
+
+
+                        // Print best students
+                        Console.Write("Enter the name of the best student(s) to print (separated by commas): ");
+                        string inputprint = Console.ReadLine();
+
+                        string[] names = inputprint.Split(',');
+
+                        Console.WriteLine("Best students:");
+
+                        foreach (string name in names)
+                        {
+                            if (students.Contains(name.Trim()))
+                            {
+                                Console.WriteLine("- " + name.Trim());
+                                Console.WriteLine("");
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("- " + name.Trim() + " is not found in the student list.");
+                            }
+                        }
+
+
+                        // Print award-winning students
+                        Console.WriteLine("Enter the names of the award-winning students (or 'done' to continue): ");
+                        string inputval = Console.ReadLine();
+                        while (inputval.ToLower() != "done")
+                        {
+                            if (students.Contains(inputval))
+                            {
+                                Console.WriteLine(inputval + " is an award-winning student!");
+                                Console.WriteLine("");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Could not find " + inputval + " in the college.");
+                            }
+
+                            Console.WriteLine("Enter the name of an award-winning student (or 'done' to continue): ");
+                            inputval = Console.ReadLine();
+                        }
+
+                        Console.WriteLine("Thanks for using the admission program!");
+                        Console.ReadLine();
+
+                        break;
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ C# Training/C# Day_8/C# Day_8/Program.cs
@@ -0,0 +1,214 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace C__Day_8
+{
+    enum Designation
+    {
+        Developer,
+        QA
+    }
+
+    class Employee
+    {
+        //public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public Designation Designation { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+    public static class ValidationExtensions
+    {
+        public static bool IsValidGender(this string gender)
+        {
+            return Regex.IsMatch(gender, @"^(Male|Female|Other)$", RegexOptions.IgnoreCase);
+        }
+        public static bool IsValidEmail(this string email)
+        {
+            return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+        }
+        public static bool IsValidPhoneNumber(this string phoneNumber)
+        {
+            int Number;
+            return int.TryParse(phoneNumber, out Number) && phoneNumber.Length == 10;
+        }
+        public static bool IsValidSalary(this decimal salary)
+        {
+            return salary >= 10000 && salary <= 50000;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeeList = new List<Employee>();
+            bool continueProgram = true;
+            while (continueProgram)
+            {
+                Console.WriteLine("\nEnter 1 to add a new employee record.");
+                Console.WriteLine("Enter 2 to display all employee records.");
+                Console.WriteLine("Enter 3 to exit the application.");
+
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                switch (userInput)
+                {
+                    case 1:
+                        Employee employee = new Employee();
+
+                        Console.WriteLine("Enter First name (Required):");
+                        employee.FirstName = Console.ReadLine().Trim();
+                        while (employee.FirstName == "")
+                        {
+                            Console.WriteLine("First name is required. Please enter a valid name:");
+                            employee.FirstName = Console.ReadLine().Trim();
+                        }
+
+
+                        Console.WriteLine("Enter Last name (Required):");
+                        employee.LastName = Console.ReadLine().Trim();
+                        while (employee.LastName == "")
+                        {
+                            Console.WriteLine("Last name is required. Please enter a valid name:");
+                            employee.LastName = Console.ReadLine().Trim();
+                        }
+
+
+                        Console.WriteLine("Enter Gender (Required, Must be 'Male', 'Female', or 'Other'):");
+                        string gender = Console.ReadLine().Trim();
+                        while (!gender.IsValidGender())
+                        {
+                            Console.WriteLine("Invalid input. Please enter 'Male', 'Female', or 'Other':");
+                            gender = Console.ReadLine().Trim();
+                        }
+                        employee.Gender = gender;
+
+
+
+                        Console.WriteLine("Enter Email (Required, Must be Valid):");
+                        string email = Console.ReadLine().Trim();
+                        while (!email.IsValidEmail())
+                        {
+                            Console.WriteLine("Invalid email. Please enter a valid email address:");
+                            email = Console.ReadLine().Trim();
+                        }
+                        employee.Email = email;
+
+
+
+                        Console.WriteLine("Enter Phone number (10 digits):");
+                        string phoneNumber = Console.ReadLine().Trim();
+                        while (!phoneNumber.IsValidPhoneNumber())
+                        {
+                            Console.WriteLine("Invalid input. Please enter a 10-digit phone number:");
+                            phoneNumber = Console.ReadLine().Trim();
+                        }
+                        employee.PhoneNumber = phoneNumber;
+
+
+                        Console.WriteLine("Enter Designation (1: Developer, 2: QA,):");
+                        employee.Designation = (Designation)Convert.ToInt32(Console.ReadLine());
+
+
+                        Console.WriteLine("Enter Salary (Min 10,000 & Max 50,000):");
+                        decimal salary;
+                        var validSalary = decimal.TryParse(Console.ReadLine().Trim(), out salary);
+                        while (!validSalary || !salary.IsValidSalary())
+                        {
+                            Console.WriteLine("Invalid input. Salary should be a number between 10,000 and 50,000. Please enter a valid value:");
+                            validSalary = decimal.TryParse(Console.ReadLine().Trim(), out salary);
+                        }
+                        employee.Salary = salary;
+
+
+                        employeeList.Add(employee);
+
+                        Console.WriteLine("\nEmployee Record created successfully");
+                        //Console.WriteLine($"ID:  {employee.Id}, \nFirst Name: {employee.FirstName}, \nLast Name: {employee.LastName}, \nGender: {employee.Gender}, \nEmail: {employee.Email}, \nPhone Number: {employee.PhoneNumber}, \nDesignation: {employee.Designation}, \nSalary: {employee.Salary}");
+                        break;
+
+
+                    case 2:
+                        Console.WriteLine("\nEmployee Records:");
+                        foreach (Employee emp in employeeList)
+                        {
+                            Console.WriteLine("--------------------------------------------------------");
+                            Console.WriteLine($"\nFirst Name: {emp.FirstName}, \nLast Name: {emp.LastName}, \nGender: {emp.Gender}, \nEmail: {emp.Email}, \nPhone Number: {emp.PhoneNumber}, \nDesignation: {emp.Designation}, \nSalary: {emp.Salary}\n");
+                            Console.WriteLine("--------------------------------------------------------");
+                        }
+                        break;
+
+                    case 3:
+                        continueProgram = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input. Please enter 1, 2 or 3.");
+                        break;
+
+                }
+            }
+
+            //foreach (Employee employee in employees)
+            //{
+            //    Console.WriteLine($"ID:  {employee.Id}, \nFirst Name: {employee.FirstName}, \nLast Name: {employee.LastName}, \nGender: {employee.Gender}, \nEmail: {employee.Email}, \nPhone Number: {employee.PhoneNumber}, \nDesignation: {employee.Designation}, \nSalary: {employee.Salary}\n");
+            //}
+            //Console.WriteLine($"ID:  {employee.Id}, \nFirst Name: {employee.FirstName}, \nLast Name: {employee.LastName}, \nGender: {employee.Gender}, \nEmail: {employee.Email}, \nPhone Number: {employee.PhoneNumber}, \nDesignation: {employee.Designation}, \nSalary: {employee.Salary}");
+
+
+            //json file
+            //string employeeJson = JsonConvert.SerializeObject(employeeList, Formatting.Indented);
+            //string filePath = @"D:\GIT_C#_Training\C# Training\C# Day_8\employee.json";
+
+            //using (StreamWriter file = File.CreateText(filePath))
+            //{
+            //    file.Write(employeeJson);
+            //}
+
+            //Console.WriteLine("\nEmployee Record saved to file successfully!");
+            //Console.ReadLine();
+
+
+            string filePath = @"D:\GIT_C#_Training\C# Training\C# Day_8\employee.json";
+
+            // Check if the file already exists
+            if (File.Exists(filePath))
+            {
+                // If the file exists, read the existing data and deserialize it
+                string existingData = File.ReadAllText(filePath);
+                List<Employee> existingEmployees = JsonConvert.DeserializeObject<List<Employee>>(existingData);
+
+                // Add the new employees to the existing list
+                existingEmployees.AddRange(employeeList);
+
+                // Serialize the updated list and overwrite the existing file
+                string employeeJson = JsonConvert.SerializeObject(existingEmployees, Formatting.Indented);
+                File.WriteAllText(filePath, employeeJson);
+            }
+            else
+            {
+                // If the file doesn't exist, create a new file and write the data to it
+                string employeeJson = JsonConvert.SerializeObject(employeeList, Formatting.Indented);
+                using (StreamWriter file = File.CreateText(filePath))
+                {
+                    file.Write(employeeJson);
+                }
+            }
+
+            Console.WriteLine("\nEmployee Record saved to file successfully!");
+            Console.ReadLine();
+
+        }
+    }
+}
